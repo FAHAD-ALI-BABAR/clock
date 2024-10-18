fb708d01f303b8331ab8901aec9dbf5535214997
Fahad Ali Babar <rajafahadali931@gmail.com>
2024-10-18 17:11:03 +0500

    comm

--- Redux/counter-react-redux/src/components/Counter.jsx
+++ Redux/counter-react-redux/src/components/Counter.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Counter = () => {
   return (
-    <h1 className="display-5 fw-bold text-body-emphasis">Centered hero</h1>
+    <h1 className="display-5 fw-bold text-body-emphasis">Counter</h1>
   )
 }
 
--- Redux/counter-react-redux/src/store/index.js
+++ Redux/counter-react-redux/src/store/index.js
@@ -6,9 +6,17 @@ const initial_counter_value={
 const storereducer=(store=initial_counter_value,action)=>{
     let value=store;
     if(action.type==="Increment"){
+        console.log("increment");
+        
         value={counter: store.counter+1}
     } else if(action.type==="Decrement"){
+        console.log("Decrement");
+        
         value={counter: store.counter-1}
+    }else if(action.type==="Reset"){
+        console.log("reset");
+        value={counter: store.counter=0}
+        
     }
     return value;
 }
